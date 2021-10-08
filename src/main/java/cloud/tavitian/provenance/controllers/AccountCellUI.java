60cab84a24c3948f67c2fe96fcd54dbe28717348
Paul Tavitian <paul.tavitian@icloud.com>
2021-10-09 10:10:04 +1100

    Improved date/time and currency formatting.

--- src/main/java/cloud/tavitian/provenance/controllers/AccountCellUI.java
+++ src/main/java/cloud/tavitian/provenance/controllers/AccountCellUI.java
@@ -6,6 +6,8 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.TextAlignment;
+import java.text.NumberFormat;
+import java.util.Currency;
 
 public class AccountCellUI {
   private final HBox hBox = new HBox();
@@ -15,10 +17,15 @@ public class AccountCellUI {
   private final Label balanceLabel = new Label();
 
   public HBox gethBox(AccountResource account) {
+    Double balanceNumber = Double.parseDouble(account.getAttributes().getBalance().getValue());
+    NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance();
+    Currency currency = Currency.getInstance(account.getAttributes().getBalance().getCurrencyCode());
+    currencyFormatter.setCurrency(currency);
+    String balanceCurrencyString = currencyFormatter.format(balanceNumber);
     this.displayNameLabel.setText(account.getAttributes().getDisplayName());
     this.accountTypeLabel.setText(account.getAttributes().getAccountType().name());
     this.balanceLabel.setTextAlignment(TextAlignment.RIGHT);
-    this.balanceLabel.setText(account.getAttributes().getBalance().getValue());
+    this.balanceLabel.setText(balanceCurrencyString);
 
     vBox.getChildren().add(0, displayNameLabel);
     vBox.getChildren().add(1, accountTypeLabel);
